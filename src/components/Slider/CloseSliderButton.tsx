e1b2393915061f41f0f29fa39ad11c06b0c8b6d0
aridsonf <filhoaridson@gmail.com>
2024-04-01 01:14:25 -0300

    :lipstick: style(slider): fix close button layout to fit figma

--- src/components/Slider/CloseSliderButton.tsx
+++ src/components/Slider/CloseSliderButton.tsx
@@ -7,7 +7,13 @@ function CloseSliderButton() {
   return (
     <div className="absolute left-5 hidden lg:block md:block">
       <button
-        className="mt-5 w-10 h-10 border-white text-white rounded-full flex items-center justify-center"
+        className="
+          mt-5 ml-4 w-10 h-10 border-white text-white rounded-full
+          flex items-center justify-center bg-transparent border 
+          border-black p-2.5 cursor-pointer transition-all duration-300 
+          dark:border-white hover:border-blue-700 dark:hover:border-blue-700
+          text-white hover:text-blue-700 dark:hover:text-blue-700
+        "
         onClick={toggleSlider}
       >
         <IoIosArrowBack />
