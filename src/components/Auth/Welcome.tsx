b674c21a7ccfd8238bbacd25b3daa2d6339926ab
aridsonf <filhoaridson@gmail.com>
2024-03-31 21:54:48 -0300

    :lipstick: style(welcome): some changes to fit the figma layout

--- src/components/Auth/Welcome.tsx
+++ src/components/Auth/Welcome.tsx
@@ -1,11 +1,11 @@
 
 export default function WelcomeMessage() {
   return (
-    <div className="flex flex-col mt-5 select-none p-6">
-      <h1 className="text-3xl font-semibold text-neutral-800 dark:text-white">
+    <div className="flex flex-col gap-4 select-none py-6">
+      <h1 className="text-3xl font-semibold text-neutral-800">
         Boas-vindas!
       </h1>
-      <p className="text-lg font-medium text-gray-400 mt-5 dark:text-gray-300">
+      <p className="text-lg font-medium text-gray-600">
         Entre utilizando uma das opções abaixo
       </p>
     </div>
